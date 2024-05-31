e32f45ff16b9ff04c94436a9967eda4f62096c39
Miguel Bustamante <110345846+MiguelBusta@users.noreply.github.com>
2024-05-31 14:19:23 -0600

    23/may/24

--- mik/parsetab.py
+++ mik/parsetab.py
@@ -6,7 +6,7 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'DIV EQUAL EXP FUNCTION LPAREN MINUS NUMBER PLUS RPAREN TIMES VARIABLE\n    assignment : VARIABLE EQUAL expression \n    \n    assignment : expression\n    \n    expression : term\n    \n    expression : expression PLUS term\n    \n    expression : expression MINUS term\n    \n    term : exponent\n    \n    term : term TIMES exponent\n    \n    term : term DIV exponent\n    \n    exponent : factor\n    \n    exponent : factor EXP factor\n    \n    factor : NUMBER\n    \n    factor : VARIABLE\n    \n    factor : LPAREN expression RPAREN\n    '
+_lr_signature = 'DIV EQUAL EXP FUNCTION LPAREN MINUS NUMBER PLUS RPAREN TIMES VARIABLE\n    assignment : VARIABLE EQUAL expression\n    \n    assignment : expression\n    \n    expression : term\n    \n    expression : expression PLUS term\n    \n    expression : expression MINUS term\n    \n    term : exponent\n    \n    term : term TIMES exponent\n    \n    term : term DIV exponent\n    \n    exponent : factor\n    \n    exponent : factor EXP factor\n    \n    factor : NUMBER\n    \n    factor : VARIABLE\n    \n    factor : LPAREN expression RPAREN\n    '
     
 _lr_action_items = {'VARIABLE':([0,8,9,10,11,12,13,14,],[2,16,16,16,16,16,16,16,]),'NUMBER':([0,8,9,10,11,12,13,14,],[7,7,7,7,7,7,7,7,]),'LPAREN':([0,8,9,10,11,12,13,14,],[8,8,8,8,8,8,8,8,]),'$end':([1,2,3,4,5,6,7,16,17,18,19,20,21,22,23,],[0,-12,-2,-3,-6,-9,-11,-12,-1,-4,-5,-7,-8,-10,-13,]),'EQUAL':([2,],[9,]),'EXP':([2,6,7,16,23,],[-12,14,-11,-12,-13,]),'TIMES':([2,4,5,6,7,16,18,19,20,21,22,23,],[-12,12,-6,-9,-11,-12,12,12,-7,-8,-10,-13,]),'DIV':([2,4,5,6,7,16,18,19,20,21,22,23,],[-12,13,-6,-9,-11,-12,13,13,-7,-8,-10,-13,]),'PLUS':([2,3,4,5,6,7,15,16,17,18,19,20,21,22,23,],[-12,10,-3,-6,-9,-11,10,-12,10,-4,-5,-7,-8,-10,-13,]),'MINUS':([2,3,4,5,6,7,15,16,17,18,19,20,21,22,23,],[-12,11,-3,-6,-9,-11,11,-12,11,-4,-5,-7,-8,-10,-13,]),'RPAREN':([4,5,6,7,15,16,18,19,20,21,22,23,],[-3,-6,-9,-11,23,-12,-4,-5,-7,-8,-10,-13,]),}
 
@@ -27,17 +27,17 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> assignment","S'",1,None,None,None),
-  ('assignment -> VARIABLE EQUAL expression','assignment',3,'p_assignment_assign','translator-mik.py',66),
-  ('assignment -> expression','assignment',1,'p_assignment_expression','translator-mik.py',73),
-  ('expression -> term','expression',1,'p_expression_term','translator-mik.py',79),
-  ('expression -> expression PLUS term','expression',3,'p_expression_plus','translator-mik.py',85),
-  ('expression -> expression MINUS term','expression',3,'p_expression_minus','translator-mik.py',91),
-  ('term -> exponent','term',1,'p_term_exponent','translator-mik.py',97),
-  ('term -> term TIMES exponent','term',3,'p_term_times','translator-mik.py',103),
-  ('term -> term DIV exponent','term',3,'p_term_divides','translator-mik.py',109),
-  ('exponent -> factor','exponent',1,'p_exponent_factor','translator-mik.py',115),
-  ('exponent -> factor EXP factor','exponent',3,'p_exponent_exp','translator-mik.py',121),
-  ('factor -> NUMBER','factor',1,'p_factor_num','translator-mik.py',127),
-  ('factor -> VARIABLE','factor',1,'p_factor_variable','translator-mik.py',133),
-  ('factor -> LPAREN expression RPAREN','factor',3,'p_factor_expr','translator-mik.py',139),
+  ('assignment -> VARIABLE EQUAL expression','assignment',3,'p_assignment_assign','translator-mik.py',81),
+  ('assignment -> expression','assignment',1,'p_assignment_expression','translator-mik.py',89),
+  ('expression -> term','expression',1,'p_expression_term','translator-mik.py',95),
+  ('expression -> expression PLUS term','expression',3,'p_expression_plus','translator-mik.py',101),
+  ('expression -> expression MINUS term','expression',3,'p_expression_minus','translator-mik.py',107),
+  ('term -> exponent','term',1,'p_term_exponent','translator-mik.py',113),
+  ('term -> term TIMES exponent','term',3,'p_term_times','translator-mik.py',119),
+  ('term -> term DIV exponent','term',3,'p_term_divides','translator-mik.py',125),
+  ('exponent -> factor','exponent',1,'p_exponent_factor','translator-mik.py',131),
+  ('exponent -> factor EXP factor','exponent',3,'p_exponent_exp','translator-mik.py',137),
+  ('factor -> NUMBER','factor',1,'p_factor_num','translator-mik.py',143),
+  ('factor -> VARIABLE','factor',1,'p_factor_variable','translator-mik.py',149),
+  ('factor -> LPAREN expression RPAREN','factor',3,'p_factor_expr','translator-mik.py',155),
 ]
